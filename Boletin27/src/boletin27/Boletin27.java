1e85d83dd761bc6daba039d25c708c8d6f5abf9b
finfanterodal <finfanterodal@nautilus11.danielcastelao.org>
2019-05-06 10:10:42 +0200

    Commit inicial.

---
+++ Boletin27/src/boletin27/Boletin27.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin27;
+
+/**
+ *
+ * @author finfanterodal
+ */
+public class Boletin27 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Calculadora c1=new Calculadora();
+        c1.setVisible(true);
+        System.out.println(5*5+3);
+        String resultado="5.5";
+        Double res=Double.parseDouble(resultado);
+        System.out.println(resultado);
+        
+    }
+    
+}
